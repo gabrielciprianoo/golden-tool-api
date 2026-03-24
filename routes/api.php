f8678890963293d5cc174af1fa80a68af507c939
gabrielciprianoo <devgabrielcipriano@gmail.com>
2026-03-24 15:54:04 -0600

    🧪 test(GT04); add unit test for auth enpoints

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// 🔓 Públicas
+
 Route::post('/login', [AuthController::class, 'login']);
 
-// 🔒 Protegidas
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
-    // 🔥 EXTRA (recomendado)
+
     Route::get('/me', [AuthController::class, 'me']);
 });
