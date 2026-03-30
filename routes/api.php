8b87e4a2c1beae735f1b75144f01f47021a8a109
gabrielGG-chamba <goldengategabriel.com>
2026-03-30 14:02:45 -0600

    ✨ feat(GT04): update tool routes to use singular naming convention

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HerramientaController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HerramientaController;
-
-
-
-Route::get('/herramientas', [HerramientaController::class, 'index']);
-Route::post('/herramientas', [HerramientaController::class, 'store']);
-Route::get('/herramientas/{id}', [HerramientaController::class, 'show']);
-Route::put('/herramientas/{id}', [HerramientaController::class, 'update']);
-Route::delete('/herramientas/{id}', [HerramientaController::class, 'destroy']);
-
-Route::post('/herramientas', [HerramientaController::class, 'store']);
-Route::get('/herramientas', [HerramientaController::class, 'index']);
+Route::get('/tools', [HerramientaController::class, 'index']);
+Route::post('/tool', [HerramientaController::class, 'store']);
+Route::get('/tool/{id}', [HerramientaController::class, 'show']);
+Route::put('/tool/{id}', [HerramientaController::class, 'update']);
+Route::patch('/tool/{id}', [HerramientaController::class, 'update']);
+Route::delete('/tool/{id}', [HerramientaController::class, 'destroy']);
 
 Route::middleware('throttle:5,1')->post('/login', [AuthController::class, 'login']);
 
