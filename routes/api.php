f6a66fc12ecdef590d98f31f2957ee06004abef7
gabrielGG-chamba <goldengategabriel.com>
2026-03-31 12:33:59 -0600

    ✨ feat(GT04)inventory-backend
    middleware implement

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ToolController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tools', [ToolController::class, 'index']);
-Route::post('/tool', [ToolController::class, 'store']);
-Route::get('/tool/{id}', [ToolController::class, 'show']);
-Route::put('/tool/{id}', [ToolController::class, 'update']);
-Route::patch('/tool/{id}', [ToolController::class, 'update']);
-Route::delete('/tool/{id}', [ToolController::class, 'destroy']);
-
 Route::middleware('throttle:5,1')->post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/tools', [ToolController::class, 'index']);
+    Route::post('/tool', [ToolController::class, 'store']);
+    Route::get('/tool/{id}', [ToolController::class, 'show']);
+    Route::put('/tool/{id}', [ToolController::class, 'update']);
+    Route::patch('/tool/{id}', [ToolController::class, 'update']);
+    Route::delete('/tool/{id}', [ToolController::class, 'destroy']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::get('/me', [AuthController::class, 'me']);
