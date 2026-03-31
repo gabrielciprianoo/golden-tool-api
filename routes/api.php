a58fa83a4c2d75d164cd5a0da362fb6c250b4ab2
EdgarHuante <edgareduardohuantejeronimo@gmail.com>
2026-03-31 12:30:52 -0600

    ♻️ feat/GT05/workers-backend code improvement

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\WorkerController;
 
 Route::middleware('throttle:5,1')->post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
+     Route::get('workers', [WorkerController::class, 'index']);
+ Route::post('workers', [WorkerController::class, 'store']);
+ Route::put('workers/{worker}', [WorkerController::class, 'update']);
+ Route::delete('workers/{worker}', [WorkerController::class, 'destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('workers', WorkerController::class);
     Route::get('/me', [AuthController::class, 'me']);
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,5 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
- Route::get('workers', [WorkerController::class, 'index']);
- Route::post('workers', [WorkerController::class, 'store']);
- Route::put('workers/{worker}', [WorkerController::class, 'update']);
- Route::delete('workers/{worker}', [WorkerController::class, 'destroy']);
+
 });
